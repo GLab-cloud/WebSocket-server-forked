542f8d13028adef28d3d148171bde4414350ee04
GLab <54309362+GLab-cloud@users.noreply.github.com>
2026-02-16 09:12:12 +0000

    Server Broadcast message

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+// import { WebSocketServer, WebSocket } from "ws";
 const express = require("express");
 const enableWs = require("express-ws");
+const WebSocket = require("ws");
 
 const app = express();
 const wsinstance = enableWs(app);
+
+app.listen(8080);
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+// const wss = new WebSocketServer({ port: 8081 });
 
 // app.ws("/", (ws, req) => {
 //   console.log("connected", req.query);
@@ -41,34 +49,33 @@ const wsinstance = enableWs(app);
 //   }
 // });
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-app.listen(8080);
 //connection event
 //0:connecting
 //1:open (the only state where you can safely .send())
 //2: closing
 //3: closed
 app.ws("/", (ws, req) => {
-  ws.on("connection", (socket, request) => {
-    const ip = request.socket.remoteAddress;
-    socket.on("message", (rawData) => {
-      const message = rawData.toString();
-      console.log({ rawData });
-      wsinstance.getWss().clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(`Server Broadcast: ${message}`);
-        }
-      });
-    });
-    socket.on("error", (err) => {
-      console.error(`Error : ${err.message}: ${ip}`);
-    });
-    socket.on("close", () => {
-      console.log("Client disconnected");
+  const ip = req.socket.remoteAddress;
+  ws.on("message", (rawData) => {
+    const message = rawData.toString();
+    console.log({ rawData });
+    // Access the central WebSocket server instance
+    wsinstance.getWss().clients.forEach((client) => {
+      // Check if the client is currently open before sending the message
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(
+          `Server Broadcast message: ${message} to remote IP Address: ${ip}`
+        );
+      }
     });
   });
-}); //npm i --save-dev @types/node @types/ws
+  ws.on("error", (err) => {
+    console.error(`Error : ${err.message}: ${ip}`);
+  });
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+//npm i --save-dev @types/node @types/ws
 console.log("WebSocket Server is live on ws://whl9dn-8080.csb.app");
 // testing -> wscat -c ws://localhost:8080
