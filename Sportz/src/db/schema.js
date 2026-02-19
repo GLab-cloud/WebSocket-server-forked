e6791d71ca0b405ecb25e59409c9881840f2d58b
GLab <54309362+GLab-cloud@users.noreply.github.com>
2026-02-19 08:56:11 +0000

    Neon-db:migrate 3 tables successfully

--- Sportz/src/db/schema.js
+++ Sportz/src/db/schema.js
@@ -1,8 +1,48 @@
-import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  pgEnum,
+  jsonb,
+  integer,
+} from "drizzle-orm/pg-core";
 //Define the 'demo_User' Table
 export const demoUsers = pgTable("demo_users", {
   id: serial("id").primaryKey(),
   name: text("name").notNull(),
   email: text("email").notNull().unique(),
-  createdAt: timestamp(created_at).defaultNow().notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+export const matchStatusEnum = pgEnum("match_status", [
+  "scheduled",
+  "live",
+  "finished",
+]);
+export const matches = pgTable("matches", {
+  id: serial("id").primaryKey(),
+  sport: text("sport").notNull(),
+  homeTeam: text("home_team").notNull(),
+  awayTeam: text("away_team").notNull(),
+  status: matchStatusEnum("status").notNull().default("scheduled"),
+  endTime: timestamp("end_time"),
+  homeScore: integer("home_score").notNull().default(0),
+  awayScore: integer("away_score").notNull().default(0),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+export const commentary = pgTable("commentary", {
+  id: serial("id").primaryKey(),
+  matchId: integer("match_id")
+    .notNull()
+    .references(() => matches.id),
+  period: text("period"),
+  eventType: text("event_type"),
+  actor: text("actor"),
+  team: text("team"),
+  minute: integer("minute"),
+  sequence: integer("sequence"),
+  message: text("message").notNull(),
+  tags: text("tags").array(),
+  metadata: jsonb("metadata"),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
 });
