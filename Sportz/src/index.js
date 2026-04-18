eb64257b805181b6549b6a44d0e645bdeab7eca5
GLab <54309362+GLab-cloud@users.noreply.github.com>
2026-04-18 05:13:18 +0000

    implement Arcjet security

--- Sportz/src/index.js
+++ Sportz/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import http from "http";
 import { matchRouter } from "./routes/matches.js";
 import { attachWebSocketServer } from "./ws/server.js";
+import { securityMiddleware } from "./arcjet.js";
 const PORT = Number(process.env.PORT) || 8000;
 const HOST = Number(process.env.HOST) || "0.0.0.0";
 const app = express();
@@ -10,6 +11,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello from Express server");
 });
+//429 - http_code: for i in {1..60}; do curl -s -o /dev/nul -w "%{http_code}\n" http://localhost:8000/matches ; done
+app.use(securityMiddleware());
 app.use("/matches", matchRouter);
 const { broadcastMatchCreated } = attachWebSocketServer(server);
 //sharing function broadcastMatchCreated
